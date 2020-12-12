e715b0f913f38cd0cc8e13ddf064e62d4602fa27
Pedro Ganme <pmastudionet@gmail.com>
2020-12-12 19:49:41 -0300

    Updated gitignore and added description input

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
 
+from collections import OrderedDict 
+
 import PySimpleGUI as sg
 from serialization import save_project, open_project
 
@@ -19,7 +21,8 @@ layout = [
             [sg.Text("Project Name: ")],
             [sg.Input(key="__PROJECT_NAME__")],
             [sg.FilesBrowse('Import Clips', target='clipspaths', file_types=(("MP4 files", "*.mp4"), ("MPEG files", "*.mpeg"), ("MOV files", "*.mov"))), sg.Input(disabled=True, enable_events=True, key="clipspaths")],
-            [sg.Listbox(values=[], enable_events=True, size=(40,20), key='clipslist'), sg.Button(">", key="import"), sg.Listbox(values=[], enable_events=True, size=(40,20), key='clipslistordered')]
+            [sg.Listbox(values=[], enable_events=True, size=(40,20), key='clipslist'), sg.Button(">", key="import"), sg.Listbox(values=[], enable_events=True, size=(40,20), key='clipslistordered')],
+            [sg.Button("Setup"), sg.Button("Debug View")]
          ]
 
 
@@ -29,17 +32,30 @@ window = sg.Window('Window Title', layout)
 project = {
     "project_name": "",
     "file_name": "",
-    "imported_clips": [],
+    "imported_clips": OrderedDict(),
 }
 
 selected_clip = ""
 all_clips = []
-imported_clips = []
+imported_clips = OrderedDict()
 
 while True:
     event, values = window.read()
     if event in (None, 'Exit'):
         break
+    if event == 'Setup':
+        for c in project["imported_clips"]:
+            capt = "Decription for: " + c.split("/")[-1]
+
+            old_desc = project["imported_clips"][c]
+
+            if len(old_desc) > 0:
+                desc = sg.popup_get_text(capt, default_text=old_desc)
+            else:
+                desc = sg.popup_get_text(capt, default_text="Please give a description for this clip")
+            
+            project["imported_clips"][c] = desc
+
     if event == 'Save':
         project["project_name"] = values["__PROJECT_NAME__"]
         save_project(project)
@@ -62,9 +78,11 @@ while True:
         all_clips.remove(selected_clip)
         window['clipslist'].update(all_clips) # REMOVE FROM CURRENT LIST
 
-        project["imported_clips"].append(selected_clip) # ADD TO IMPORTED LIST
+        project["imported_clips"][selected_clip] = "" # ADD TO IMPORTED DICT
         window['clipslistordered'].update(project["imported_clips"])
         selected_clip = ""
+    if event == 'Debug View':
+        sg.popup_ok(str(project))
 
 
 # Do something with the information gathered
